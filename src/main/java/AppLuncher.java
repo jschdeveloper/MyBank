efe778174e13b9ca89f292ea4343eb4aae9959ee
Jesús Sánchez <jesus.sanchez@in-storemedia.com>
2022-11-29 12:22:09 +0100

    chore: add main and first controller

--- src/main/java/AppLuncher.java
+++ src/main/java/AppLuncher.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class AppLuncher {
+import org.apache.catalina.Context;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
+public class AppLuncher {
+
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8090);
+        tomcat.getConnector();
+
+        Context context = tomcat.addContext("", null);
+        Wrapper servlet = Tomcat.addServlet(context, "MyBankController", new MyBankController());
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/*");
+        tomcat.start();
+        System.out.println("init");
+    }
 }
--- src/main/java/MyBankController.java
+++ src/main/java/MyBankController.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MyBankController {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class MyBankController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String path = request.getRequestURI();
+
+        System.out.printf("path : {%s}", path);
+
+        if ("/".equalsIgnoreCase(path)) {
+            response.getWriter().write("Hola");
+            System.out.println();
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
 }
